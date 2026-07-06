a7157a286e42e8c79ee48bed51887c0cd3d68313
jgeiger310-code <jgeiger310@gmail.com>
2026-07-06 16:08:18 -0400

    Add MASICS manifest Dropbox locator fallbacks

--- assets/config.js
+++ assets/config.js
@@ -2,6 +2,10 @@ window.MASICS_DROPBOX_CONFIG = Object.freeze({
   appKey: "1p4bbydzkh0wblg",
   redirectUri: "https://jgeiger310-code.github.io/masics-mario-cloud-viewer/",
   manifestDropboxPath: "id:PzJJcyLjOoMAAAAAAAH0yQ",
+  manifestDropboxPathAlternates: [
+    "/JakeAI_Exchange/MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json",
+    "ns:14586613299//MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json"
+  ],
   queueIdentity: "masics_mario_task026_rc53_636_v1",
   queueVersion: "MASICS_MARIO_QUEUE_MANIFEST_V1",
   expectedRecordCount: 636,
