2d161a307b2fd957f21beab3a3d44f8692d79944
jgeiger310-code <jgeiger310@gmail.com>
2026-07-06 16:08:36 -0400

    Use namespace path for MASICS manifest

--- assets/config.js
+++ assets/config.js
@@ -1,10 +1,10 @@
 window.MASICS_DROPBOX_CONFIG = Object.freeze({
   appKey: "1p4bbydzkh0wblg",
   redirectUri: "https://jgeiger310-code.github.io/masics-mario-cloud-viewer/",
-  manifestDropboxPath: "id:PzJJcyLjOoMAAAAAAAH0yQ",
+  manifestDropboxPath: "ns:14586613299//MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json",
   manifestDropboxPathAlternates: [
-    "/JakeAI_Exchange/MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json",
-    "ns:14586613299//MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json"
+    "id:PzJJcyLjOoMAAAAAAAH0yQ",
+    "/JakeAI_Exchange/MegaBuild/06_PROTECTED_RUNTIME/MASICS_MARIO_CLOUD_VIEWER/MASICS_MARIO_QUEUE_MANIFEST_V1.json"
   ],
   queueIdentity: "masics_mario_task026_rc53_636_v1",
   queueVersion: "MASICS_MARIO_QUEUE_MANIFEST_V1",
